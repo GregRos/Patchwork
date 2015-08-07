595c5d7e5d1c5b691d4702f732a7125f507bce8c
GregRos <life.gregr@gmail.com>
2015-08-07 09:43:08 +0300

    Documentation added

--- Patchwork/AssemblyPatcher/NewMemberStatus.cs
+++ Patchwork/AssemblyPatcher/NewMemberStatus.cs
@@ -1,6 +1,16 @@
 namespace Patchwork {
+	/// <summary>
+	/// This used as a return value from methods that create new stuff. It is used in AssemblyPatcher.cs
+	/// in order to remove items from further processing if the need arises.
+	/// </summary>
 	internal enum NewMemberStatus {
+		/// <summary>
+		/// Everything is A-okay.
+		/// </summary>
 		Continue,
+		/// <summary>
+		/// The last item wasn't created correctly. Remove it from further processing.
+		/// </summary>
 		InvalidItem
 	}
 }
