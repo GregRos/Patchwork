08e73604018a658224230e6732d681db7359a8fb
GregRos <life.gregr@gmail.com>
2015-08-29 21:46:39 +0300

    Allow multiple DisablePatchingByName

--- Patchwork.Attributes/PatchingAttributes/DisablePatchingByNameAttribute.cs
+++ Patchwork.Attributes/PatchingAttributes/DisablePatchingByNameAttribute.cs
@@ -16,7 +16,7 @@ namespace Patchwork.Attributes {
 	/// <summary>
 	/// This attribute disables patching types and/or methods in the assembly based on a regex you supply, which is matched against their full name (including namespace and declaring type).
 	/// </summary>
-	[AttributeUsage(AttributeTargets.Assembly)]
+	[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
 	public class DisablePatchingByNameAttribute : Attribute {
 		public string Regex {
 			get;
