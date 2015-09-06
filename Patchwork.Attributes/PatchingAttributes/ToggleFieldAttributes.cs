676bbe706bb54627f0fc86840a133a8a1290a677
GregRos <life.gregr@gmail.com>
2015-09-06 22:31:38 +0300

    Minor changes

--- Patchwork.Attributes/PatchingAttributes/ToggleFieldAttributes.cs
+++ Patchwork.Attributes/PatchingAttributes/ToggleFieldAttributes.cs
@@ -23,40 +23,4 @@ namespace Patchwork.Attributes {
 		}
 	}
 
-	/// <summary>
-	/// This toggles (or XORs) all the specified decleration attributes in the method. Lets you add/remove things like 'sealed and 'virtual''.
-	/// Must be used with ModifiesMemberAttribute.
-	/// </summary>
-	public class ToggleMethodAttributes : PatchingAttribute {
-		public MethodAttributes Attributes {
-			get;
-		}
-
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="attributes">The attributes to toggle. Default to 0 (no attributes).</param>
-		public ToggleMethodAttributes(MethodAttributes attributes = 0) {
-			Attributes = attributes;
-		}
-	}
-
-	/// <summary>
-	/// This toggles (or XORs) all the specified decleration attributes in the type. Lets you add/remove things like 'sealed'.
-	/// Must be used with ModifiesMemberAttribute.
-	/// Not implemented.
-	/// </summary>
-	internal class ToggleTypeAttributes : PatchingAttribute {
-		public TypeAttributes Attributes {
-			get;
-		}
-
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="attributes">The attributes to toggle. Default to 0 (no attributes).</param>
-		private ToggleTypeAttributes(TypeAttributes attributes = 0) {
-			Attributes = attributes;
-		}
-	}
 }
